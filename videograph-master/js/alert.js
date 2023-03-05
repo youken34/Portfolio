ba118c4d1e549a7901531a4098f400cc22ddd431
Comies34 <come.bonal@gmail.com>
2023-03-05 00:49:25 -0800

    k

--- videograph-master/js/alert.js
+++ videograph-master/js/alert.js
@@ -46,7 +46,6 @@ function createCustomAlert(txt) {
 }
 
 function removeCustomAlert() {
-    console.log("ok");
     nav = document.getElementsByTagName("nav")[0].style.filter = "blur(0px)";
     for (let i = 0; i < document.getElementsByTagName("nav").length; i++) { // loop through each element
         document.getElementsByTagName("nav")[i].style.filter = "blur(0px)";
@@ -75,4 +74,63 @@ function hide(ElementDisplayed) {
     for (var i = 0; i < ElementDisplayed.length; i++) {
         ElementDisplayed[i].style.display = 'none';
     }
+}
+
+function fill() {
+    const creation = document.querySelector('.creation:before');
+    creation.classList.add('animation-done');
+}
+
+function redirection() {
+    window.location.href = "/Portfolio/videograph-master/portfolio.html";
+}
+
+function grow(image, numberFlex) {
+    const body = document.body;
+    var flex = document.getElementsByClassName("flex")[numberFlex];
+    switch (image) {
+        case 'clean':
+            var clicked = false;
+            var element = document.getElementById('clean');
+            if (element.style.transform === 'scale(1)') {
+                undo();
+                element.style.transform = 'scale(2)';
+                element.style.zIndex = "2";
+            } else {
+                element.style.transform = 'scale(1)';
+                element.style.zIndex = "1";
+            }
+            break;
+        case 'castel':
+            var element = document.getElementById('castel');
+            if (element.style.transform === 'scale(1)') {
+                undo();
+                element.style.transform = 'scale(2)';
+                element.style.zIndex = "2";
+            } else {
+                element.style.transform = 'scale(1)';
+                element.style.zIndex = "1";
+            }
+            break;
+
+    }
+}
+
+function undo() {
+    var flex = document.querySelectorAll(".mix");
+    flex.forEach(element => {
+        element.style.transform = 'scale(1)';
+        element.style.zIndex = "1";
+    });
+}
+
+function hideAll(numberFlex) {
+    const elements = document.querySelectorAll('section');
+
+    // Loop through the elements and apply the CSS rule
+    elements.forEach(element => {
+        element.style.opacity = "0.5";
+    });
+
+
 }
